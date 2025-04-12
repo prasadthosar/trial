35cdbb0596ad3f303aa95e61e23b2b5fb227b1dc
prasadthosar <122084654+prasadthosar@users.noreply.github.com>
2025-04-12 17:29:36 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Install Chrome
+apt-get update
+apt-get install -y wget unzip gnupg
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d. -f1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -q "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/
+rm chromedriver_linux64.zip
